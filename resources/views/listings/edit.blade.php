e06abd97b3fe6294fee60f59fb45dd725a78ec14
Cas1311 <cashoogerbeets@gmail.com>
2024-07-11 23:10:18 +0200

    edit form

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -10,97 +10,93 @@
         <form method="POST" action="/listings/{{ $listing->id }}" enctype="multipart/form-data">
             @csrf
             @method('PUT')
-            <div class="mb-6">
-                <label for="petBreed" class="inline-block text-lg mb-2">Pet Breed</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="petBreed"
-                    value="{{ $listing->petBreed }}" placeholder="Example: Labradoodle" />
+            <div class="listing-section">
+                <label for="petBreed" class="question-header">Pet Breed</label>
+                <input type="text" class="question-box" name="petBreed" value="{{ $listing->petBreed }}"
+                    placeholder="Example: Labradoodle" />
             </div>
 
             @error('petBreed')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="hourRate" class="inline-block text-lg mb-2">Hour Rate</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="hourRate"
-                    value="{{ $listing->hourRate }}" placeholder="Example: $15" />
+            <div class="listing-section">
+                <label for="hourRate" class="question-header">Hour Rate</label>
+                <input type="text" class="question-box" name="hourRate" value="{{ $listing->hourRate }}"
+                    placeholder="Example: $15" />
             </div>
 
             @error('hourRate')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="location" class="inline-block text-lg mb-2">location</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                    value="{{ $listing->location }}" placeholder="Example: P Sherman 42 Wallaby Way, Sydney" />
+            <div class="listing-section">
+                <label for="location" class="question-header">location</label>
+                <input type="text" class="question-box" name="location" value="{{ $listing->location }}"
+                    placeholder="Example: P Sherman 42 Wallaby Way, Sydney" />
             </div>
 
             @error('location')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="startDate" class="inline-block text-lg mb-2">
+            <div class="listing-section">
+                <label for="startDate" class="question-header">
                     Starting Date
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="startDate"
-                    value="{{ $listing->startDate }}" />
+                <input type="text" class="question-box" name="startDate" value="{{ $listing->startDate }}" />
             </div>
 
             @error('startDate')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="endDate" class="inline-block text-lg mb-2">
+            <div class="listing-section">
+                <label for="endDate" class="question-header">
                     Ending Date
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="endDate"
-                    value="{{ $listing->endDate }}" />
+                <input type="text" class="question-box" name="endDate" value="{{ $listing->endDate }}" />
             </div>
 
             @error('endDate')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email"
-                    value="{{ $listing->email }}" />
+            <div class="listing-section">
+                <label for="email" class="question-header">Contact Email</label>
+                <input type="text" class="question-box" name="email" value="{{ $listing->email }}" />
             </div>
 
             @error('email')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="petName" class="inline-block text-lg mb-2">Pet Name</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="petName"
-                    value="{{ $listing->petName }}" />
+            <div class="listing-section">
+                <label for="petName" class="question-header">Pet Name</label>
+                <input type="text" class="question-box" name="petName" value="{{ $listing->petName }}" />
             </div>
 
             @error('petName')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="tags" class="inline-block text-lg mb-2">
+            <div class="listing-section">
+                <label for="tags" class="question-header">
                     Tags (Comma Separated)
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-                    value="{{ $listing->tags }}" placeholder="Example: Dog, Cat, Playful, etc" />
+                <input type="text" class="question-box" name="tags" value="{{ $listing->tags }}"
+                    placeholder="Example: Dog, Cat, Playful, etc" />
             </div>
 
             @error('tags')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="picture" class="inline-block text-lg mb-2">
+            <div class="listing-section">
+                <label for="picture" class="question-header">
                     Picture
                 </label>
-                <input type="file" class="border border-gray-200 rounded p-2 w-full" name="picture" />
+                <input type="file" class="question-box" name="picture" />
             </div>
             <img class="w-48 mr-6 mb-6"
                 src="{{ $listing->picture ? asset('storage/' . $listing->picture) : asset('/images/no-image.png') }}"
@@ -110,20 +106,20 @@
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <label for="description" class="inline-block text-lg mb-2">
+            <div class="listing-section">
+                <label for="description" class="question-header">
                     Description/Important Needs
                 </label>
-                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" value="{{ $listing->description }}"
-                    rows="10" placeholder="Include neccesary description, important needs, etc"></textarea>
+                <textarea class="question-box" name="description" value="{{ $listing->description }}" rows="10"
+                    placeholder="Include neccesary description, important needs, etc"></textarea>
             </div>
 
             @error('description')
                 <p class="text-red-500" text-xs mt-1>{{ $message }}</p>
             @enderror
 
-            <div class="mb-6">
-                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+            <div class="listing-section-bottom">
+                <button class="button">
                     Edit Listing
                 </button>
 
