6590cdaf5ed82a1bca12cefde90f68c39f353807
Cas1311 <cashoogerbeets@gmail.com>
2024-07-12 11:45:17 +0200

    fixed search form

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,9 +1,11 @@
-<div class="search-container">
-    <div class="search-icon">
-        <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+<form action="/">
+    <div class="search-container">
+        <div class="search-icon">
+            <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+        </div>
+        <input type="text" name="search" class="search-input" placeholder="Search Petsitting Jobs..." />
+        <div class="search-button">
+            <button class="button" type="submit">Search</button>
+        </div>
     </div>
-    <input type="text" name="search" class="search-input" placeholder="Search Petsitting Jobs..." />
-    <div class="search-button">
-        <button class="button" type="submit">Search</button>
-    </div>
-</div>
+</form>
